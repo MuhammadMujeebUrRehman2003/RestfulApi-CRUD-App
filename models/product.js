207fe04fc240478914c7ad919035044f4c597d93
MUJEEB UR REHMAN <muhammadmujeeburrehman613@gmail.com>
2024-05-14 23:29:13 -0700

    first commit

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    Date: {
+        type: Date,
+        default: Date.now
+    },
+
+})
+
+const product = mongoose.model("product", productSchema)
+
+module.exports = product;
